257d309b97b1e6e5097347728ebdfccb70e9ee56
unknown <mwinamu@gmail.com>
2025-06-18 17:11:15 +0300

    add

--- Startup.cs
+++ Startup.cs
@@ -52,7 +52,7 @@ namespace MyPaginationAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
