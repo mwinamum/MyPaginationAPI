1d8183a8f68d348dffeed45916f586d52b278e31
unknown <mwinamu@gmail.com>
2025-06-21 01:42:36 +0300

    Adds add and update methods

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -4,6 +4,7 @@ namespace MyPaginationAPI
 {
     public class WeatherForecast
     {
+        public int Id { get; set; }
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
